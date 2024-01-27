b981239589381943979f0c7581859bbefe90ae68
Daniela Baron <baron.daniela@gmail.com>
2024-01-27 10:22:28 -0500

    enhnace feature test to verify New search link

--- spec/features/weather_search_spec.rb
+++ spec/features/weather_search_spec.rb
@@ -2,12 +2,14 @@ require "rails_helper"
 
 describe "Weather Search" do
   let(:weather_response) { build(:weather_response) }
+  let(:weather_client_instance) { instance_double(Weather::Client) }
 
-  it "user searches for weather" do
-    weather_client_instance = instance_double(Weather::Client)
+  before do
     allow(Weather::Client).to receive(:new).and_return(weather_client_instance)
     allow(weather_client_instance).to receive(:today).and_return(weather_response)
+  end
 
+  it "displays weather results from search and clears out on clicking New" do
     visit root_path
 
     fill_in "address", with: "paris"
@@ -17,6 +19,13 @@ describe "Weather Search" do
     expect(page).to have_content("Temperature: 11.0°C / 51.8°F")
     expect(page).to have_content("Condition: Partly cloudy")
     expect(page).to have_content("Air Quality: Good")
+
+    click_on "New Search"
+
+    expect(page).to have_no_content("Current weather for Paris, Ile-de-France, France")
+
+    expect(page).to have_field("address")
+    expect(page).to have_button("Get Forecast")
   end
 
   it "displays an error message when no address provided" do
